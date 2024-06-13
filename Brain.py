536be7091230a5748fb3f49efa05060aa786d480
Angel Yair Mendez Sandoval <angelyairmendezsandoval@gmail.com>
2024-06-12 23:31:45 -0600

    implemented bucket function in my brain

--- Brain.py
+++ Brain.py
@@ -1,1 +1,10 @@
 # I first want to calculate the volume of a bucket
+from Math_Formulas.Bucket_Formula import calculate_bucket_volume
+
+# Set bucket dimensions
+upper_radius = 29  # cm, inner radius of bucket
+lower_radius = 21  # cm, inner radius of bucket
+bucket_height = 27  # cm
+units = "cm"
+bucket_volume = calculate_bucket_volume(upper_radius, lower_radius, bucket_height)
+print("The bucket volume is: ", bucket_volume, units, "^2")
